3a0cf0861c65392b3e71d302f50ad1d361265fe6
Administrator <2084267015@qq.com>
2019-11-06 21:22:09 +0800

    gradle

--- manager/src/main/java/com/luo/manager/controller/ProductController.java
+++ manager/src/main/java/com/luo/manager/controller/ProductController.java
@@ -2,14 +2,23 @@ package com.luo.manager.controller;
 
 import com.luo.entity.Product;
 import com.luo.manager.service.ProductService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private static Logger logger = LoggerFactory.getLogger(ProductController.class);
     @Autowired
     private ProductService productService;
 
@@ -19,4 +28,30 @@ public class ProductController {
 
         return result;
     }
+    @GetMapping(value = "/{id}")
+    public Product get(@PathVariable("id") String id) {
+        logger.debug("根据id查询");
+        Product result = productService.findOne(id);
+        logger.debug("current product:{}",result);
+        return result;
+    }
+    @GetMapping(value = "")
+    public  Page<Product> getAll(String ids, BigDecimal minRewardRate, BigDecimal maxRewardRate, String status,
+                          @RequestParam(defaultValue = "0") int pageNum,
+                          @RequestParam(defaultValue = "10") int pageSize) {
+        logger.debug("多条件查询");
+        List<String> idList=null,statusList=null;
+        if (!StringUtils.isEmpty(ids)) {
+            idList = Arrays.asList(ids.split(","));
+        }
+
+        if (!StringUtils.isEmpty(status)) {
+            statusList = Arrays.asList(status.split(","));
+        }
+
+        Pageable pageable = new PageRequest(pageNum,pageSize);
+        Page<Product> page = productService.query(idList, minRewardRate, maxRewardRate, statusList, pageable);
+        return page;
+    }
+
 }
--- manager/src/main/java/com/luo/manager/service/ProductService.java
+++ manager/src/main/java/com/luo/manager/service/ProductService.java
@@ -1,18 +1,22 @@
 package com.luo.manager.service;
 
 import com.luo.entity.Product;
-import com.luo.entity.enums.OrderStatus;
-import com.luo.entity.enums.OrderType;
 import com.luo.entity.enums.ProductStatus;
 import com.luo.manager.repositories.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import javax.persistence.criteria.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -21,13 +25,50 @@ public class ProductService {
     private ProductRepository productRepository;
 
     public Product addProduct(Product product) {
-logger.info("创建产品");
+        logger.info("创建产品");
         //数据校验
         checkProduct(product);
         setDefault(product);
         return productRepository.save(product);
     }
 
+    public Product findOne(String id) {
+        Assert.notNull(id,"id 不能为空");
+        Product one = productRepository.findOne(id);
+        return one;
+    }
+
+    //付咋查询
+    public Page<Product> query(List<String> idList, BigDecimal minRate, BigDecimal maxRate,
+                               List<String> statusList, Pageable pageable) {
+        Specification<Product> specification = new Specification<Product>() {
+            @Override
+            public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                Expression<String> idCol = root.get("id");
+                Expression<String> statusCol = root.get("status");
+                Expression<BigDecimal> rewardRateCol = root.get("rewardRate");
+                List<Predicate> predicateList = new ArrayList<>();
+                if (idList != null && idList.size() > 0) {
+                    predicateList.add(idCol.in(idList));
+                }
+                if (BigDecimal.ZERO.compareTo(minRate) < 0) {
+                    predicateList.add(cb.ge(rewardRateCol, minRate));
+                }
+
+                if (BigDecimal.ZERO.compareTo(maxRate) < 0) {
+                    predicateList.add(cb.le(rewardRateCol, maxRate));
+                }
+                if (idList != null && idList.size() > 0) {
+                    predicateList.add(idCol.in(idList));
+                }
+                query.where(predicateList.toArray(new Predicate[0]));
+                return null;
+            }
+        };
+
+        Page<Product> all = productRepository.findAll(specification, pageable);
+        return all;
+    }
     private void setDefault(Product product) {
 
         if (product.getCreateAt() == null) {
