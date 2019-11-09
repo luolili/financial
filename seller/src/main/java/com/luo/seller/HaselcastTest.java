67f86df8c347300afacaf6c5740cc0115ca36018
Administrator <2084267015@qq.com>
2019-11-09 15:04:44 +0800

    hazelcast

---
+++ seller/src/main/java/com/luo/seller/HaselcastTest.java
@@ -0,0 +1,24 @@
+package com.luo.seller;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+
+@Component
+public class HaselcastTest {
+
+    @Autowired
+    private HazelcastInstance hazelcastInstance;
+
+    @PostConstruct
+    public void test() {
+        Map map = hazelcastInstance.getMap("mee");
+        ((IMap<Object, Object>) map).put("name", "mee");
+
+
+    }
+}
